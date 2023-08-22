005f690d18e17ddb83dd641fcdd147252b88bf66
Franklin Mensah <fiificode@gmail.com>
2023-08-22 20:29:06 +0000

    Signup and login ui screens

--- screens/Login.js
+++ screens/Login.js
@@ -1,14 +1,229 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  Alert,
+  Pressable,
+} from "react-native";
+import React, { useState } from "react";
+import COLORS from "../constants/colors";
+import { Ionicons } from "@expo/vector-icons";
+import Checkbox from "expo-checkbox";
+import Button from "../components/Button";
 
-const Login = () => {
+export default function Login({navigation}) {
+  const [isPasswordShown, setIsPasswordShown] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
-  )
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: COLORS.white,
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          marginHorizontal: 22,
+        }}
+      >
+        <View style={{ marginVertical: 22 }}>
+          <Text
+            style={{
+              fontSize: 32,
+              fontWeight: "bold",
+              marginVertical: 12,
+              marginTop: 40,
+              color: COLORS.black,
+            }}
+          >
+            Hey, Welcome back!
+          </Text>
+
+          <Text style={{ fontSize: 16, color: COLORS.black }}>
+            Hello again, you have been missed!
+          </Text>
+        </View>
+        <View style={{ marginBottom: 12 }}>
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: 600,
+              marginVertical: 8,
+            }}
+          >
+            Email address
+          </Text>
+          <View
+            style={{
+              width: "100%",
+              height: 48,
+              borderColor: COLORS.black,
+              borderWidth: 1,
+              borderRadius: 8,
+              alignItems: "center",
+              justifyContent: "center",
+              paddingLeft: 22,
+            }}
+          >
+            <TextInput
+              placeholder="Enter your email address"
+              placeholderTextColor={COLORS.grey}
+              keyboardType="email-address"
+              style={{ width: "100%" }}
+            />
+          </View>
+        </View>
+        <View style={{ marginBottom: 12 }}>
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: 600,
+              marginVertical: 8,
+            }}
+          >
+            Password
+          </Text>
+          <View
+            style={{
+              width: "100%",
+              height: 48,
+              borderColor: COLORS.black,
+              borderWidth: 1,
+              borderRadius: 8,
+              alignItems: "center",
+              justifyContent: "center",
+              paddingLeft: 22,
+            }}
+          >
+            <TextInput
+              placeholder="Enter your password"
+              placeholderTextColor={COLORS.grey}
+              secureTextEntry={isPasswordShown}
+              style={{ width: "100%" }}
+            />
+            <TouchableOpacity
+              onPress={() => setIsPasswordShown(!isPasswordShown)}
+              style={{
+                position: "absolute",
+                right: 12,
+              }}
+            >
+              {isPasswordShown === true ? (
+                <Ionicons name="eye-off" size={24} color={COLORS.black} />
+              ) : (
+                <Ionicons name="eye" size={24} color={COLORS.black} />
+              )}
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={{ flexDirection: "row", marginVertical: 6 }}>
+          <Checkbox
+            style={{ marginRight: 8 }}
+            value={isChecked}
+            onValueChange={setIsChecked}
+            color={isChecked ? COLORS.primary : undefined}
+          />
+          <Text>Remember Me</Text>
+        </View>
+        <Button
+          title="Login"
+          filled
+          style={{ marginTop: 18, marginBottom: 4 }}
+        />
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            marginVertical: 20,
+          }}
+        >
+          <View
+            style={{
+              flex: 1,
+              height: 1,
+              backgroundColor: COLORS.grey,
+              marginHorizontal: 10,
+            }}
+          />
+          <Text>Or Login with</Text>
+          <View
+            style={{
+              flex: 1,
+              height: 1,
+              backgroundColor: COLORS.grey,
+              marginHorizontal: 10,
+            }}
+          />
+        </View>
+        <View style={{ flexDirection: "row", justifyContent: "center" }}>
+          <TouchableOpacity
+            onPress={() => Alert.alert("Sign up with facebook")}
+            style={{
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "row",
+              height: 52,
+              borderWidth: 1,
+              borderColor: COLORS.grey,
+              marginRight: 4,
+              borderRadius: 10,
+            }}
+          >
+            <Image
+              source={require("../assets/facebook.png")}
+              style={{
+                height: 36,
+                width: 36,
+                marginRight: 8,
+              }}
+              resizeMode="contain"
+            />
+            <Text>Facebook</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => Alert.alert("Sign up with Google")}
+            style={{
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "row",
+              height: 52,
+              borderWidth: 1,
+              borderColor: COLORS.grey,
+              marginRight: 4,
+              borderRadius: 10,
+            }}
+          >
+            <Image
+              source={require("../assets/google.png")}
+              style={{
+                height: 30,
+                width: 30,
+                marginRight: 8,
+              }}
+              resizeMode="contain"
+            />
+            <Text>Google</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={{
+          flexDirection:"row",
+          justifyContent:"center",
+          marginVertical:22
+        }}>
+          <Text style={{fontSize:16,color:COLORS.black}}>Don't have an account?</Text>
+          <Pressable onPress={()=>navigation.navigate("Signup")}>
+            <Text style={{fontSize:16,color:COLORS.primary,fontWeight:"bold",marginLeft:6}}>Signup</Text>
+          </Pressable>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
-
-export default Login
-
-const styles = StyleSheet.create({})
